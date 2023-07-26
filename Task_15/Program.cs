2871582cf59b13dc22e28127141a509bead12cc0
Dima Bykadorov <dimka.b@live.com>
2023-07-26 12:32:46 +0300

    add Task 15

---
+++ Task_15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите цифру от 1 до 7: ");
+int numOfDay = int.Parse(Console.ReadLine());
+if (numOfDay >= 1 && numOfDay <= 7){
+    if (numOfDay == 6 || numOfDay == 7) Console.WriteLine("Ура, выходной!");
+    else Console.WriteLine("Опять будни...");
+}
+else Console.WriteLine("Некорректный ввод");
