e331a5fc642c4896c1d0e758528fa6f7c2dec271
Dima Bykadorov <dimka.b@live.com>
2023-07-26 13:57:25 +0300

    add string solution

--- Task_10/Program.cs
+++ Task_10/Program.cs
@@ -1,3 +1,13 @@
-﻿int number = new Random().Next(100, 1000);
+﻿//Математическое решение
+int number = new Random().Next(100, 1000);
 int result = (number / 10) % 10;
 Console.WriteLine($"Вторая цифра числа {number} это {result}");
+
+// Решение со строкой
+/*Console.Write("Введите трёхзначное число: ");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+if (numberInt >= 100 && numberInt < 1000){
+    Console.WriteLine($"Вторая цифра числа {number} это {number[1]}");
+}
+else Console.WriteLine($"Число {number} не яляется трёхзначным");*/
--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -1,4 +1,5 @@
-﻿int number = new Random().Next(0, 100000);
+﻿//Математическое решение
+int number = new Random().Next(0, 100000);
 int result = 0;
 if (number < 100) Console.WriteLine($"В числе {number} нет третьей цифры");
 else if (number >= 100 && number < 1000) {
@@ -13,3 +14,12 @@ else {
     result = ((number % 1000) / 100);
     Console.WriteLine($"Третья цифра числа {number} это {result}");
 }
+
+// Решение со строкой
+/*Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int numberInt = int.Parse(number);
+if (numberInt >= 100 ){
+    Console.WriteLine($"Третья цифра числа {number} это {number[2]}");
+}
+else Console.WriteLine($"В числе {number} нет третьей цифры");*/
