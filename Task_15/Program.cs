0c96912415bbfa6c7efb370a2fcf13a7a6226ba7
Dima Bykadorov <dimka.b@live.com>
2023-07-26 14:01:00 +0300

    correct problem

--- Task_15/Program.cs
+++ Task_15/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Введите цифру от 1 до 7: ");
-int numOfDay = int.Parse(Console.ReadLine());
+int numOfDay = int.Parse(Console.ReadLine()!);
 if (numOfDay >= 1 && numOfDay <= 7){
     if (numOfDay == 6 || numOfDay == 7) Console.WriteLine("Ура, выходной!");
     else Console.WriteLine("Опять будни...");
